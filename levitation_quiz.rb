2cba1b86190a3dd7944aede64f1b6bc0f8be6b35
Sachin Mahajan <sachinm78@gmail.com>
2019-02-25 21:39:55 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -5,8 +5,7 @@ def levitation_quiz
         answer = gets.chomp
         break if answer == "Wingardium Leviosa"
     end
-    puts "Okay, okay, jeez. I'll stop. Sorry."
-end
+    puts "You passed the quiz!" 
 end
 
 
